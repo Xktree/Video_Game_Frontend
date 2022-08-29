5e86f51da3ae6ad56869ac20130074110052fda4
Devin Chen <taunwe79@gmail.com>
2022-08-29 11:42:28 -0500

    moved to a new repository due to package dependency conflicts; installed axios + react-google-charts + created components folder under /src and added child folders with files

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import './App.css';
+
+function App() {
+
+
+export default App;
---
+++ src/main.js
@@ -0,0 +1,31 @@
+// User Stories
+
+// Total story points: /65
+
+// (5 points) As a developer, I want to have good, consistent commits.
+
+// (5 points) As a developer, I want to build an aesthetically pleasing user interface.
+
+// (10 points) As a developer, I want to utilize the Java Spring Rest API controller and service layers to store my endpoints. (DONE)
+
+// - Endpoint: GET request to get all video game records (DONE)
+
+// - Endpoint: GET request to get a single video game record by ID (DONE)
+
+// (5 points) As a developer, I want to create a React.js application to consume and visualize the Spring Rest API video game API data. (DONE)
+
+// (5 points) As a developer, I want to visualize the API data in the form of line graphs or charts (using React Google Charts)
+
+// (10 points) As a video game enthusiast, I want to be able to see a data visualization (chart or graph) of which video game console is best to invest in based on the number of game copies sold globally on that console since 2013.
+
+// (5 points) As a developer, I want to write a sample evaluation question of my own that can be answered by analysis of the API data.
+
+// (10 points) As a video game enthusiast, I want to see a data visualization of the analyzed data regarding the developer created sample evaluation question.
+
+// (10 points) As a video game enthusiast, I want to be able to search for a game and see its details.
+
+// Bonus
+
+// (5 points) As a video game enthusiast, I want to be able to search for a game and see a data visualization (chart or graph) of the number of copies sold per console.
+
+// (5 points) As a video game enthusiast, I want to see a data visualization of which publishers have had the most success per console.
